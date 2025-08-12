0b1af145aff7081b18d4eb6b0cca47b9839e4af8
Sedigh Hamid Reza <H_Sedigh@iranet.net>
2025-08-12 12:18:45 +0330

    'dev'

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,13 @@
+//src/services/api.js
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:8080'
+    baseURL: process.env.REACT_APP_API_URL,
+    headers: {
+    'Content-Type': 'application/json',
+    // مثلا اگر توکن داری اینجا می‌تونی بذاری (بعداً)
+    // Authorization: `Bearer ${token}`
+  }
 })
 
 export default api;
